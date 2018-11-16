b3efea1828512eeca4508d5990df8463fa444432
Vincent <vincentm5@yahoo.fr>
2018-11-16 14:11:47 +0100

    ajout d'une salle

--- rooms/models.py
+++ rooms/models.py
@@ -5,9 +5,14 @@ class Salle(models.Model):
     ressource_type = models.CharField(max_length=200)
     localisation = models.CharField(max_length=200)
     capacite = models.IntegerField(default=1)
+     def __str__(self):
+        return self.nom
 
 class Reservation(models.Model):
     salle = models.ForeignKey(Salle, on_delete=models.CASCADE)
     titre = models.CharField(max_length=200)
     date_debut = models.DateTimeField('date de debut')
     date_fin = models.DateTimeField('date de fin')
+    
+    def __str__(self):
+        return self.salle
