ef659569be2503a35aebaa0b336f49cf11a3d525
Vincent <vincentm5@yahoo.fr>
2018-11-16 17:38:48 +0100

    deconnection / connection user

--- EscapeEmAll/urls.py
+++ EscapeEmAll/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView 
 
 urlpatterns = [
-    url(r'^rooms/', include('rooms.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^user/',  include('django.contrib.auth.urls') ),
+    url(r'^', TemplateView.as_view(template_name='home.html'), name='home'),
+    
 ]
