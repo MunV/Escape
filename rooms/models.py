fece3df11e2f7f3350408027bfc13d01007c991d
Vincent <vincentm5@yahoo.fr>
2018-11-16 13:57:01 +0100

    models

--- rooms/models.py
+++ rooms/models.py
@@ -1,6 +1,13 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class Salle(models.Model):
+    nom = models.CharField(max_length=200)
+    ressource_type = models.CharField(max_length=200)
+    localisation = models.CharField(max_length=200)
+    capacite = models.IntegerField(default=1)
+
+class Reservation(models.Model):
+    salle = models.ForeignKey(Salle, on_delete=models.CASCADE)
+    titre = models.CharField(max_length=200)
+    date_debut = models.DateTimeField('date de debut')
+    date_fin = models.DateTimeField('date de fin')
